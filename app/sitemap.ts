33b443b1d22aec9c04e58ba787f9ae6faa06e460
GAMORA ONLINE <admin@gamora-online.com>
2026-08-26 13:53:36 +0300

    Add all products to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,19 +6,24 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   const products = await getProducts();
 
-  return [
+  const productUrls: MetadataRoute.Sitemap = products.map((product) => ({
+    url: `${baseUrl}/product/${product.id}`,
+    changeFrequency: "weekly",
+    priority: 0.8,
+  }));
+
+  const staticUrls: MetadataRoute.Sitemap = [
     {
       url: baseUrl,
-      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
-
-    ...products.map((product) => ({
-      url: `${baseUrl}/product/${product.id}`,
-      lastModified: new Date(),
-      changeFrequency: "weekly" as const,
-      priority: 0.8,
-    })),
+    {
+      url: `${baseUrl}/contact`,
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
   ];
+
+  return [...staticUrls, ...productUrls];
 }
